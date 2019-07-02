dcb0a2c460ceeed445715670a772552bf1854cf4
manashmndl <manashmndl@gmail.com>
2019-07-02 17:56:57 +0600

    IIAFE removed

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 const config = require('./config/config.json');
 
 
-(async () => {
-    
-
+async function runChrome() {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+
+    const mailAddress = await axios.get(config.mailAPI.getNewMail).then(d => d.data);
 
     await page.setViewport({width: 1280, height: 800});
     await page.goto(config.urls.oreilly.url);
@@ -26,6 +26,32 @@ const config = require('./config/config.json');
 
     await page.waitForSelector(config.urls.oreilly.registerButtonSelector, {visible: true});
     await page.click(config.urls.oreilly.registerButtonSelector);
+}
 
-    // await page.close();
-})()
+runChrome();
+// (async () => {
+    
+
+//     const browser = await puppeteer.launch({headless: false});
+//     const page = await browser.newPage();
+
+//     await page.setViewport({width: 1280, height: 800});
+//     await page.goto(config.urls.oreilly.url);
+
+//     await page.waitForSelector(config.urls.oreilly.firstNameSelector);
+//     await page.type(config.urls.oreilly.firstNameSelector, "MANASH");
+
+//     await page.waitForSelector(config.urls.oreilly.lastNameSelector);
+//     await page.type(config.urls.oreilly.lastNameSelector, "MANDAL");
+
+//     await page.waitForSelector(config.urls.oreilly.emailSelector);
+//     await page.type(config.urls.oreilly.emailSelector, "helloworldz");
+
+//     await page.waitForSelector(config.urls.oreilly.passwordSelector);
+//     await page.type(config.urls.oreilly.passwordSelector, "@ccess1234");
+
+//     await page.waitForSelector(config.urls.oreilly.registerButtonSelector, {visible: true});
+//     await page.click(config.urls.oreilly.registerButtonSelector);
+
+//     // await page.close();
+// })()
